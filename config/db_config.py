959645445605d8c60b09fff391537b222f160288
Ashutosh Kumar Singh <127626579+Ashu-ksingh@users.noreply.github.com>
2025-11-13 19:34:52 +0530

    config updated

---
+++ config/db_config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'retail_db',
+    'user': 'postgres',
+    'password': 'ashu2909'  # change if your PostgreSQL password is different
+}
